cb02466f53da30e808a03cd3f430ecca366db3c2
Nico Stranquist <nstranquist@gmail.com>
2021-12-09 21:49:44 -0600

    finish animations

--- notes/04-animation/src/script.js
+++ notes/04-animation/src/script.js
@@ -1,5 +1,6 @@
 import './style.css'
 import * as THREE from 'three'
+import gsap from 'gsap'
 
 // Most animations work like "stop motion"... make change, re-render
 // - typically doing 60fps
@@ -12,6 +13,13 @@ import * as THREE from 'three'
 
 // Need to adapt to not make animation dependent on the frame rate
 // - can use Date.now()
+
+// Should use logical Clock provided by three.js
+// - consider not using 'getDelta()' method from Clock
+
+// Can use library like GSAP to have more control, tweens, timelines, etc.
+// - npm i gsap@version
+// - gsap maintains its own tick, so no need for clock logic, but still have to manually re-render scene each frame
 
 
 // Canvas
@@ -44,21 +52,25 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height)
 renderer.render(scene, camera)
 
-let time = Date.now();
+const clock = new THREE.Clock()
+
+// gsap.to(mesh.position, { duration: 1, delay: 1, x: 2 })
+// gsap.to(mesh.position, { duration: 1, delay: 2, x: 0 })
 
 // Animations
 const tick = () => {
     // console.log('loop')
 
-    const currentTime = Date.now();
-    const deltaTime = currentTime - time;
-    time = currentTime; // update time for next frame
+    const elapsedTime = clock.getElapsedTime();
 
-    console.log(deltaTime)
+    console.log(elapsedTime)
 
     // update objects
     // mesh.position.x += 0.01
-    mesh.rotation.y += 0.001 * deltaTime
+    // mesh.rotation.y = elapsedTime;
+    mesh.rotation.y = elapsedTime * Math.PI / 2 // rotate 1/4 time per second
+    mesh.position.y = Math.sin(elapsedTime)
+    mesh.position.x = Math.cos(elapsedTime)
 
     // render
     renderer.render(scene, camera)
